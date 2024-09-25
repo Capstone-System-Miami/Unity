c3177d2b17846c191b98d85487f22d09da6afc8b
Layla H <laylahoey22@gmail.com>
2024-09-24 20:52:28 -0400

    Basic Attributes System

--- System Miami/Assets/_Project/_Scripts/_Character/PLAYER/CharacterSheet.cs
+++ System Miami/Assets/_Project/_Scripts/_Character/PLAYER/CharacterSheet.cs
@@ -2,11 +2,45 @@ using UnityEngine;
 
 namespace SystemMiami
 {
+    public enum CharacterClassType { FIGHTER, MAGE, ROGUE, TANK };
+
     public class CharacterSheet : MonoBehaviour
     {
+        #region VARS
+        //===============================
+
+        [SerializeField] private CharacterClassType _characterClass;
+        [SerializeField] private BaseAttributes[] _baseAttributes;
+
+        [SerializeField] private AttributeSet _attributes;
+
+        public AttributeSet Attributes { get { return _attributes; } }
+
+        //===============================
+        #endregion
+
+        #region PRIVATE METHODS
+        //===============================
+
         void Awake()
         {
-
+            assignAttributes();
         }
+
+        private void assignAttributes()
+        {
+            BaseAttributes classBaseAttributes = _baseAttributes[(int)_characterClass];
+
+            _attributes = new AttributeSet(classBaseAttributes);
+        }
+
+        //===============================
+        #endregion
+
+        #region PUBLIC METHODS
+        //===============================
+
+        //===============================
+        #endregion
     }
 }
