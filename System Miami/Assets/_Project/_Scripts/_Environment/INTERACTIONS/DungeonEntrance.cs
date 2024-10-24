dd1b4b8858d5173af4138c28ac8a61b6245a03e0
Layla H <laylahoey22@gmail.com>
2024-10-24 12:02:51 -0400

    Dungeon Entrance presets example
    - Find the scene under _Scenes > Door Trigger
    - Changes the "sign" block color based on the set difficulty.
    - Changes the light color based on the difficulty.
    - These colors / sprites are determined by scriptable object presets, which the generator could use to assign Dungeon Difficulty at runtime while loading the level.

--- System Miami/Assets/_Project/_Scripts/_Environment/INTERACTIONS/DungeonEntrance.cs
+++ System Miami/Assets/_Project/_Scripts/_Environment/INTERACTIONS/DungeonEntrance.cs
@@ -1,18 +1,38 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.Tilemaps;
 
 namespace SystemMiami
 {
     public class DungeonEntrance : MonoBehaviour
     {
-        private DifficultyLevel _difficulty;
+        /// <summary>
+        /// This wouldn't be serialized in the final version --
+        /// it would be set at runtime by the Generator script.
+        /// </summary>
+        [SerializeField] private DifficultyLevel _difficulty;
 
+        [SerializeField] private Light2D _light;
         private Color _lightColor = Color.white;
 
         [SerializeField] private Tilemap _signTilemap;
         [SerializeField] private Vector3Int _signTilePosition;
         private TileBase _tile;
         private Color _tileColor;
+
+        /// <summary>
+        /// This would ideally be on the generator script
+        /// </summary>
+        [SerializeField] private DungeonEntrancePreset[] _presets;
+
+        // This wouldn't be here in the final version --
+        // these fns would be called from the Generator script.
+        private void Start()
+        {
+            LoadPreset(_presets[(int)_difficulty]);
+            SetLight();
+            SwapTileForSign();
+        }
 
         public void LoadPreset(DungeonEntrancePreset preset)
         {
@@ -28,9 +48,9 @@ namespace SystemMiami
             _signTilemap.color = _tileColor;
         }
 
-        public void LightOn()
+        public void SetLight()
         {
-            //etc...
+            _light.color = _lightColor;
         }
     }
 }
