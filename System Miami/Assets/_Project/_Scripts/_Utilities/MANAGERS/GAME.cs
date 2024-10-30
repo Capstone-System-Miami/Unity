a9e1974f022929b82819f516fceb16deb6cb6a7b
Layla H <laylahoey22@gmail.com>
2024-10-30 14:38:03 -0400

    First Playable

--- System Miami/Assets/_Project/_Scripts/_Utilities/MANAGERS/GAME.cs
+++ System Miami/Assets/_Project/_Scripts/_Utilities/MANAGERS/GAME.cs
@@ -4,6 +4,7 @@ using SystemMiami.CombatSystem;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 namespace SystemMiami.Management
 {
@@ -11,9 +12,32 @@ namespace SystemMiami.Management
     {
         public Action<Combatant> CombatantDeath;
 
+        [SerializeField] int _dungeonIndex;
+        [SerializeField] int _neighborhoodIndex;
+
         protected override void Awake()
         {
             base.Awake();
         }
+        private void switchScene(int buildIndex)
+        {
+            SceneManager.LoadScene(buildIndex);
+        }
+
+        private void switchScene(string buildName)
+        {
+            SceneManager.LoadScene(buildName);
+        }
+
+        public void GoToDungeon()
+        {
+            switchScene(_dungeonIndex);
+        }
+
+        public void GoToNeighborhood()
+        {
+            switchScene(_neighborhoodIndex);
+        }
+
     }
 }
