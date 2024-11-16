d53d3dcc4d0e92f0927cc5555670466e0745eeec
Evethalie <148084606+Evethalie@users.noreply.github.com>
2024-11-16 16:03:46 -0500

    Started Working on ability anims

---
+++ System Miami/Assets/_Project/_Prefabs/_ABILITIES/_Magical/Arcane Wave/AbilityDirections.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SystemMiami
+{
+    [Serializable]
+    public class AbilityDirections 
+    {
+        public AnimationClip Up;
+        public AnimationClip UpRight;
+        public AnimationClip Right;
+        public AnimationClip DownRight;
+        public AnimationClip Down;
+        public AnimationClip DownLeft;
+        public AnimationClip Left;
+        public AnimationClip UpLeft;
+
+    }
+}
