755b0de70a5501d5175375bd5b407f5bf0f9187d
Evethalie <148084606+Evethalie@users.noreply.github.com>
2024-12-26 17:25:09 -0500

    return

--- System Miami/Assets/_Project/Items/Scripts/Base/QuickslotButtonUI.cs
+++ System Miami/Assets/_Project/Items/Scripts/Base/QuickslotButtonUI.cs
@@ -8,15 +8,18 @@ using UnityEngine.UI;
         public int index;
         public Image Icon;
         [SerializeField] Combatant player;
+        public bool locked;
         private void Start()
         {
             UpdateQuickslotInventory();
             int ID = gameObject.GetInstanceID();
             Debug.LogWarning($"{ID}");
+           locked = false;
         }
 
         public void UpdateQuickslotInventory()
         {
+            if (locked) return;
             if (Inventory.instance.quickslot[index].itemData != null)
             {
                 item = Inventory.instance.quickslot[index];
