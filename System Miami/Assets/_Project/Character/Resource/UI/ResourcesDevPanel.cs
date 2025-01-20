2b75a19080fb2579028c8c15d06cc48b79f16113
Cuchiii <alecdq2004@gmail.com>
2025-01-20 16:09:41 -0500

    stats and ability bars done AD
    linked ability bars and stats with character

--- System Miami/Assets/_Project/Character/Resource/UI/ResourcesDevPanel.cs
+++ System Miami/Assets/_Project/Character/Resource/UI/ResourcesDevPanel.cs
@@ -8,12 +8,12 @@ namespace SystemMiami
     {
         [SerializeField] private Combatant _player;
 
-        [SerializeField] private TextBox _panelLabel;
+       /** [SerializeField] private TextBox _panelLabel; 
 
         [SerializeField] private LabeledField _health;
         [SerializeField] private LabeledField _stamina;
         [SerializeField] private LabeledField _mana;
-        [SerializeField] private LabeledField _speed;
+        [SerializeField] private LabeledField _speed;*/
         [SerializeField] private Image _healthImage;
         [SerializeField] private Image _staminaImage;
         [SerializeField] private Image _manaImage;
@@ -44,54 +44,54 @@ namespace SystemMiami
 
         private void initializeLabels()
         {
-            _panelLabel.SetForeground("Resources Dev Panel");
+           /* _panelLabel.SetForeground("Resources Dev Panel");
 
             _health.Label.SetForeground("Health:");
             _stamina.Label.SetForeground("Stamina:");
             _mana.Label.SetForeground("Mana:");
-            _speed.Label.SetForeground("Speed:");
+            _speed.Label.SetForeground("Speed:");*/
         }
 
         private void updateHealth()
         {
-            if (_health == null) { return; }
+           /* if (_health == null) { return; }
 
             string result = $"{_player.Health.Get()} / {_player.Health.GetMax()}";
 
-            _health.Value.SetForeground(result);
+            _health.Value.SetForeground(result);         */
 
             _healthImage.fillAmount = _player.Health.Get() / _player.Health.GetMax();
         }
 
         private void updateStamina()
         {
-            if (_stamina == null) { return; }
+           /* if (_stamina == null) { return; }
 
             string result = $"{_player.Stamina.Get()} / {_player.Stamina.GetMax()}";
 
-            _stamina.Value.SetForeground(result);
+            _stamina.Value.SetForeground(result); */
 
-            _staminaImage.fillAmount = _player.Stamina.Get() / _player.Stamina.GetMax();
+            _staminaImage.fillAmount = _player.Stamina.Get() / _player.Stamina.GetMax(); 
         }
 
         private void updateMana()
         {
-            if (_mana == null) { return; }
+          /*  if (_mana == null) { return; }
 
             string result = $"{_player.Mana.Get()} / {_player.Mana.GetMax()}";
 
-            _mana.Value.SetForeground(result);
+            _mana.Value.SetForeground(result); */
 
             _manaImage.fillAmount = _player.Mana.Get() / _player.Mana.GetMax();
         }
 
         private void updateSpeed()
         {
-            if (_speed == null) { return; }
+           /* if (_speed == null) { return; }
 
             string result = $"{_player.Speed.Get()} / {_player.Speed.GetMax()}";
 
-            _speed.Value.SetForeground(result);
+            _speed.Value.SetForeground(result); */
 
             _speedImage.fillAmount = _player.Speed.Get() / _player.Speed.GetMax();
         }
