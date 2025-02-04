9c3e9f830abfc87941b01ec2a62f34b13c108f4a
Layla H <laylahoey22@gmail.com>
2025-02-04 01:18:39 -0500

    Made a quick `Quit` button
    It was time

--- System Miami/Assets/_Project/Neighborhood/Scenes/CharacterMenuToggle.cs
+++ System Miami/Assets/_Project/Neighborhood/Scenes/CharacterMenuToggle.cs
@@ -6,6 +6,10 @@ public class CharacterMenuToggle : MonoBehaviour
 
     void Update()
     {
+        /// TODO: This might be impossible to test unless built.
+        /// Escape unfocuses the "Game" tab when you're in Play
+        /// mode in the Editor so that you can change things
+        /// in the inspector.
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             ToggleMenu();
