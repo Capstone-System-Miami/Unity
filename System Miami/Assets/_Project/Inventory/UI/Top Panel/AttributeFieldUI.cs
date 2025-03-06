ac45a90a4e4f8fb0719daaee65a2540ba5a73728
Layla H <laylahoey22@gmail.com>
2025-03-06 01:50:18 -0500

    Tiny bit of reformatting

--- System Miami/Assets/_Project/Inventory/UI/Top Panel/AttributeFieldUI.cs
+++ System Miami/Assets/_Project/Inventory/UI/Top Panel/AttributeFieldUI.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 namespace SystemMiami
 {
-    public class AttributeFieldUI : LabeledField
+    public class AttributeFieldUI : MonoBehaviour
     {
-        [SerializeField] TextBox PreviewValue;
+        //[Header("Internal Refs")]        
+        //[SerializeField, Tooltip(
+        //    "Required. This LabeledField field should contain the " +
+        //    " actual Label with the name of the Attribute")]
+        //private LabeledField CurrentField;
+
+        //[SerializeField, Tooltip(
+        //    "Not required. Drag something here if you decide to " +
+        //    "add an actual display box for the amount that an " +
+        //    "Attribute is being upgraded by.")]
+        //private LabeledField Upgrade;
+
+        //[SerializeField] private LabeledField Preview;
+
+        [SerializeField] protected TextBox hi;
+
+        private void SetHeaders()
+        {
+        }
     }
 }
