2a717dcc7555db6bf5e0bfcbb7eed372d47c97d0
unknown <Joseph.Hoey001@mymdc.net>
2025-03-05 19:26:44 -0500

    not much just dont remember what I changedbefore talking abotu art ugh

---
+++ System Miami/Assets/_Project/Inventory/UI/Top Panel/AttributeFieldUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SystemMiami
+{
+    public class AttributeFieldUI : LabeledField
+    {
+        [SerializeField] TextBox PreviewValue;
+    }
+}
