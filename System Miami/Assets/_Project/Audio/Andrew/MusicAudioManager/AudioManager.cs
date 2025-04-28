c50ce127d8a2e44ed443a212b9191d52b4d37afc
softyay <laylahoey22@gmail.com>
2025-04-28 05:16:26 -0400

    Boss Dungeon Music

--- System Miami/Assets/_Project/Audio/Andrew/MusicAudioManager/AudioManager.cs
+++ System Miami/Assets/_Project/Audio/Andrew/MusicAudioManager/AudioManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.Audio;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using SystemMiami.Management;
 using SystemMiami.Utilities;
@@ -159,6 +160,11 @@ namespace SystemMiami
             musicSource.Play();
         }
 
+        public void PlayBossMusic()
+        {
+            PlayMusic(dungeonHigh);
+        }
+
 
         public float GetMainVolumePercent()
         {
@@ -201,6 +207,16 @@ namespace SystemMiami
         protected virtual void HandleSceneLoaded(Scene scene, LoadSceneMode mode)
         {
             if (overrideBuildIndex) { return; }
+
+            // Play boss music if necessary
+            if (scene.name == GAME.MGR.DungeonSceneName
+                && GAME.MGR.TryGetEnemies(out List<GameObject> combs)
+                && combs.Where(enemy => enemy.TryGetComponent(out BossTag boss))
+                    .ToList().Count > 0)
+            {
+                AudioManager.MGR.PlayBossMusic();
+                return;
+            }
 
             sceneMusic = allMusic.Where(music => music.BuildIndex == scene.buildIndex).ToArray();
 
